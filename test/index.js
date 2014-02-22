e6b4088f6692f0c2cc9ad4f1198ca1c39ee79143
psirenny <djtorres0@gmail.com>
2014-02-22 13:10:06 -0500

    clean up tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,39 @@
 var _ = require('lodash')
-  , chai = require('chai')
   , fixtures = require('./fixtures')
   , mocha = require('mocha')
-  , negamax = require('..').negamax;
+  , negamax = require('..').negamax
+  , should = require('chai').should();
 
 describe('negamax', function () {
-  it('asdf', function () {
-    console.log(negamax(fixtures.tictactoe.blank));
-    console.log(negamax(fixtures.tictactoe.p1winhor));
-    console.log(negamax(fixtures.tictactoe.p1winver));
-    console.log(negamax(fixtures.tictactoe.p1windag));
-    console.log(negamax(fixtures.tictactoe.p1win2));
+  it('is a function', function () {
+    negamax.should.be.a('function');
+  });
+
+  it('should return a node', function () {
+    negamax(fixtures.tictactoe.blank).node.should.be.an('object');
+  });
+
+  it('should return a score', function () {
+    negamax(fixtures.tictactoe.blank).score.should.be.a('number');
+  });
+
+  it('should not win on a blank tic-tac-toe board', function () {
+    negamax(fixtures.tictactoe.blank).score.should.equal(0);
+  });
+
+  it('should win horizontally on this tic-tac-toe board', function () {
+    negamax(fixtures.tictactoe.p1winhor).score.should.equal(1);
+  });
+
+  it('should win vertically on this tic-tac-toe board', function () {
+    negamax(fixtures.tictactoe.p1winver).score.should.equal(1);
+  });
+
+  it('should win diagonally on this tic-tac-toe board', function () {
+    negamax(fixtures.tictactoe.p1windag).score.should.equal(1);
+  });
+
+  it('should win after two moves on this tic-tac-toe board', function () {
+    negamax(fixtures.tictactoe.p1win2).score.should.equal(1);
   });
 });
